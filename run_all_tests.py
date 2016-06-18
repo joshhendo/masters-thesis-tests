f4b9b13512e1ad0160a412e9ff5cfe366ef44a9f
Josh <joshhendo@outlook.com>
2016-06-18 16:39:03 +1000

    Changing batch script to python script

---
+++ run_all_tests.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+import shlex
+
+def run_command(cmd):
+	cmd_args = shlex.split(cmd)
+	print(cmd_args)
+	subprocess.call(cmd_args)
+
+for x in range(0,5):
+	run_command("python test_basic_with.py");
+	run_command("python test_basic_withouy.py");
--- run_all_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..5}
-do
-	python test_basic_with.py
-	python test_basic_without.py
-done
-
-python process_results.py
